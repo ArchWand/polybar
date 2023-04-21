d7463d9740d28025ee3f23bde03dd7beae391898
ArcWandx86 <arcwandx86@gmail.com>
2023-04-21 14:11:43 -0400

    New update.sh to prompt more descriptive updates

--- update.sh
+++ update.sh
@@ -1,13 +1,31 @@
-if [ "$1" = "-d" ]; then
+#!/bin/zsh
+# Make sure script is run from the directory it is in
+cd "$(dirname "$0")"
+
+# Update directories that cannot be hard linked
+function up_dir {
+	rsync -a --delete $1 .
+}
+
+# If the script is called without parameters,
+# allow for manual committing
+if [[ -z "$1" ]]; then
+	exit
+fi
+
+# Check diffs
+if [[ "$1" = "-d" ]]; then
 	git diff
 	exit
 fi
 
-# If first parameter is empty, default message
-git add .
-if [ -z "$1" ]; then
-	git commit -m "Update polybar config"
-else
-	git commit -m "$1"
+# Auto-commit everything
+if [[ -a "$1" ]]; then
+	git add .
+	if [[ -z "$2" ]]; then
+		git commit -m "Update dotfiles"
+	else
+		git commit -m "$2"
+	fi
+	git push
 fi
-git push
