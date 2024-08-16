b1702cc1fcb5ef1deaddd9e92f2f64a68347d8a9
Robert Zhu <arcwandx86@gmail.com>
2024-08-16 08:47:50 -0400

    AMD

--- scripts/power.sh
+++ scripts/power.sh
@@ -1,1 +1,1 @@
-upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep energy-rate | awk '{printf "%2.2f W", $2}'
+upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep energy-rate | awk '{printf "%2.2f W", $2}'
